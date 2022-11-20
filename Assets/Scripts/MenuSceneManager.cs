42244fc8e143f8e8e95ec89decf55116a8d62efd
biyasto <biyasto@gmail.com>
2022-11-20 12:51:59 +0700

    Menu SM script

---
+++ Assets/Scripts/MenuSceneManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangePlayScene()
+    {
+        SceneManager.LoadScene("ExampleScene");
+    }
+}
